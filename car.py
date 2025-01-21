35d11224bd5a2ca2ecafb53f0ef8df33156c1341
Luiz <purez@Luiz-93Pureza.>
2025-01-21 22:17:44 +0000

    2

--- car.py
+++ car.py
@@ -1,13 +1,32 @@
-print ("Hello World")
-
 class cars:
+    def __init__(self, make, model, year, color, maxSpeed, fuelLevel=100):
 
-    def __init__(self):
-        current_speed = 1
-        max_speed = 1
-        fuel_level = 100
+        self.make = make
+        self.model = model
+        self.year = year
+        self.color = color
 
-        if current_speed > max_speed:
-            current_speed = max_speed
-            max_speed = current_speed
-            fuel_level = max_speed
+
+        self.currentSpeed = 10
+        self.maxSpeed = maxSpeed
+        self.fuelLevel = fuelLevel
+
+    def accelerate(self, amount):
+
+        if self.fuelLevel > 0:
+            self.currentSpeed += amount
+            self.currentSpeed = min(self.currentSpeed, self.maxSpeed)
+            self.fuelLevel -= amount
+            self.fuelLevel = max(self.fuelLevel, 0)
+        else:
+            self.currentSpeed = 0
+
+    def brake(self, amount):
+
+        self.currentSpeed -= amount
+        self.currentSpeed = max(self.currentSpeed, 0)
+
+    def refuel(self, amount):
+
+        self.fuelLevel += amount
+        self.fuelLevel = min(self.fuelLevel, 100)
--- main.py
+++ main.py
@@ -1,13 +1,21 @@
-print ("Hello World")
+from car import cars
 
-class cars:
+car1 = cars("BMW", "Series 3", 2022, "White", 180)
+car2 = cars("Audi", "A4", 2021, "Black", 200)
 
-    def __init__(self):
-        current_speed = 1
-        max_speed = 1
-        fuel_level = 100
 
-        if current_speed > max_speed:
-            current_speed = max_speed
-            max_speed = current_speed
-            fuel_level = max_speed
+car1.accelerate(100)
+car1.accelerate(2)
+car1.brake(0)
+car1.refuel(0)
+
+car2.accelerate(1)
+car2.refuel(2)
+car2.brake(4)
+
+
+
+print(f"\nMake: {car1.make}, Model: {car1.model}, Current Speed: {car1.currentSpeed}, Fuel Level: {car1.fuelLevel}")
+
+
+print(f"\nMake: {car2.make}, Model: {car2.model}, Current Speed: {car2.currentSpeed}, Fuel Level: {car2.fuelLevel}")
