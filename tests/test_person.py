fd281e9f94fcee270fdb36150a70a341e2487260
0purel51 <0purel51@solent.ac.uk>
2025-01-28 10:51:37 +0000

    test.py testing and adding functions

--- tests/test_person.py
+++ tests/test_person.py
@@ -8,3 +8,7 @@ def test_create_person():
 def test_sad():
     p1 = Person("abc", 150, 50, 40)
     assert p1.hunger == 50
+
+def test_run_return_false_energy():
+    p1 = Person("abc", 150, 50, 40)
+    assert p1.run(20) == False
